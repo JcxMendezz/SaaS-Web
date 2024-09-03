b9ebac9bc2d445c82dc65b2137a27dce8f00c33b
JcxMendezz <juanmendez17t@gmail.com>
2024-09-03 12:52:34 -0500

    feature: End, Api, Website, Fonts, Styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from "next";
-import LocalFont from "next/font/local";
+import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "./components/dashboard/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
-const CaskaydiaMonoNerdFontMono = LocalFont({
-  src: "./fonts/CaskaydiaMonoNerdFontMono-Regular.ttf",
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
 });
 
 export const metadata: Metadata = {
@@ -18,7 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={CaskaydiaMonoNerdFontMono.className}>{children}</body>
+      <body className={`${geistSans.className} ${geistMono.className}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster richColors closeButton />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
