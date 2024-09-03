cae8834a4b3297ec2117d0f3d76bd76db7d39f21
JcxMendezz <juanmendez17t@gmail.com>
2024-09-03 11:57:45 -0500

    feature: Components Shadcn.iu, + RoutesCreate + Utils + Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import LocalFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const CaskaydiaMonoNerdFontMono = LocalFont({
+  src: "./fonts/CaskaydiaMonoNerdFontMono-Regular.ttf",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={CaskaydiaMonoNerdFontMono.className}>{children}</body>
     </html>
   );
 }
